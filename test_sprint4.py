d48a2d26784a3b329dfe7b4df412297a5259255a
denisqa <denisqa153@yandex.ru>
2026-03-16 11:32:53 +0300

    Фиксы в соответствии с комментариями ревьюера

--- test_sprint4.py
+++ test_sprint4.py
@@ -1,25 +1,20 @@
 import pytest
 from main import BooksCollector
+@pytest.fixture
+def books_collector():
+    return BooksCollector()
 
-class TestBooksCollector: #проверка настройки для создания книги
-    def test_create_book(self):
-        books_collector = BooksCollector()
-        assert books_collector.books_genre == {}
-        assert books_collector.favorites == []
-        assert books_collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
-        assert books_collector.genre_age_rating == ['Ужасы', 'Детективы']
-
-    def test_add_new_book_success_created(self): #проверка добавления книги в словарь books_genre
-        books_collector = BooksCollector()
+class TestBooksCollector: 
+    def test_add_new_book_success_created(self, books_collector): #проверка добавления книги в словарь books_genre
+        
         name = 'Нига'
         books_collector.add_new_book(name)
         assert name in books_collector.books_genre 
         assert books_collector.books_genre[name] == ''
     
 
-    def test_set_book_genre_establish_genre(self): #проверка установления жанра книге
+    def test_set_book_genre_establish_genre(self, books_collector): #проверка установления жанра книге
 
-        books_collector = BooksCollector()
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -29,9 +24,8 @@ class TestBooksCollector: #проверка настройки для созда
 
         assert books_collector.books_genre[name] == genre 
 
-    def test_get_book_genre_output_genre(self): # проверка получения жанра книги по имени
+    def test_get_book_genre_output_genre(self, books_collector): # проверка получения жанра книги по имени
 
-        books_collector = BooksCollector()
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -42,8 +36,8 @@ class TestBooksCollector: #проверка настройки для созда
         assert result == genre
 
 
-    def test_get_books_genre_output_books_genre(self): # проверка получения словаря books_genre
-        books_collector = BooksCollector()
+    def test_get_books_genre_output_books_genre(self, books_collector): # проверка получения словаря books_genre
+        
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -54,8 +48,8 @@ class TestBooksCollector: #проверка настройки для созда
         assert result == books_collector.books_genre
     
 
-    def test_add_book_in_favorites_added_favorites(self): #проверка добавления книги в избранное
-        books_collector = BooksCollector()
+    def test_add_book_in_favorites_added_favorites(self, books_collector): #проверка добавления книги в избранное
+        
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -66,8 +60,8 @@ class TestBooksCollector: #проверка настройки для созда
         assert name in books_collector.favorites
     
 
-    def test_delete_book_from_favorites_deleted_favorites(self): #проверка удаления книги из избранного
-        books_collector = BooksCollector()
+    def test_delete_book_from_favorites_deleted_favorites(self, books_collector): #проверка удаления книги из избранного
+        
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -80,9 +74,9 @@ class TestBooksCollector: #проверка настройки для созда
         assert name not in books_collector.favorites
     
 
-    def test_get_list_of_favorites_books_output_favorites(self):     #проверка вывода избранного списка
+    def test_get_list_of_favorites_books_output_favorites(self, books_collector):     #проверка вывода избранного списка
 
-        books_collector = BooksCollector()
+        
         name = 'Нига'
         genre = 'Фантастика'
 
@@ -92,12 +86,12 @@ class TestBooksCollector: #проверка настройки для созда
 
         result = books_collector.get_list_of_favorites_books()
         
-        assert result == books_collector.favorites
+        assert result == [name]
 
 
 
-    def test_get_books_for_children_book_fits_baby(self): #проверка возврата книг, которые подходят детям
-        books_collector = BooksCollector()
+    def test_get_books_for_children_book_fits_baby(self, books_collector): #проверка возврата книг, которые подходят детям
+        
 
         name = 'Нига'
         genre = 'Фантастика'
@@ -108,8 +102,7 @@ class TestBooksCollector: #проверка настройки для созда
         assert result == [name]
 
     
-    def test_get_books_for_children_book_not_fits_baby(self): #проверка возврата книг, которые не подходят детям
-        books_collector = BooksCollector()
+    def test_get_books_for_children_book_not_fits_baby(self, books_collector): #проверка возврата книг, которые не подходят детям
 
         name = 'Нига'
         genre = 'Ужасы'
@@ -124,8 +117,7 @@ class TestBooksCollector: #проверка настройки для созда
     "genre",
     ["Фантастика", "Комедии", "Мультфильмы"]
 )
-    def test_get_books_with_specific_genre_output_specific_genre(self,genre): #проверка вывода книги с определенным жанром
-        books_collector = BooksCollector()
+    def test_get_books_with_specific_genre_output_specific_genre(self,genre,books_collector): #проверка вывода книги с определенным жанром
 
         name = 'Нига'
 
